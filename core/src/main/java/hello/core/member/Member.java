626fb60cd4a75e586ea236e73e0471b5c975cd0e
EunsilChoi92 <atheung1@naver.com>
2022-02-20 23:38:16 +0900

    member 패키지 구성중

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -6,15 +6,33 @@ public class Member {
     private String name;
     private Grade grade;
 
-    public Member(Long id) {
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Member(String name) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public Member(Grade grade) {
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
         this.grade = grade;
     }
 }
