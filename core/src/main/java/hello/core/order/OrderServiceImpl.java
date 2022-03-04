c43aa133b1bab09c6cefc8496828943fbe417ef5
EunsilChoi92 <atheung1@naver.com>
2022-03-04 23:40:18 +0900

    새로운 할인 정책 적용과 문제점

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,6 +11,28 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPilicy();
 
+    /*
+    고정 할인 정책에서 정률 할인 정책으로 변경하려면 코드를
+    private final DiscountPolicy discountPolicy = new RateDiscountPilicy();
+    로 변경해야 함
+
+    <문제점>
+    역할과 구현을 충실하게 분리했고, 다형성을 활용하고 인터페이스와 구현 객체를 분리했지만
+    OCP, DIP와 같은 객체지향 설계 원칙을 충실히 준수하지 않았음
+    1.  DIP 위반
+        - OrderServiceImpl은 DiscoutPolicy 인터페이스에 의존하면서 DIP를 지킨 것 같지만
+        추상(인터페이스)뿐만 아니라 구현 클래스에도 의존하고 있음
+            - 추상(인터페이스) 의존  : DiscountPolicy
+            - 구현 클래스 : FixDiscountPolicy, RateDiscutPolicy
+    2. OCP 위반
+        - 지금 코드는 기능을 확장에서 변경하면 클라이언트 코드(OrderServiceImpl)에 영향을 줌
+            - OrderServiceImpl에서 private final DiscountPolicy discountPolicy = new FixDiscountPilicy();를
+            private final DiscountPolicy discountPolicy = new RateDiscountPilicy(); 로 변경하는 행위
+    
+    <해결방안>
+    누군가가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야 함
+    */
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
