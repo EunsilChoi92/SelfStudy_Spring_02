444dd06e5f6aec176a564cd63e43cd6c78427ff2
EunsilChoi92 <atheung1@naver.com>
2022-03-14 19:34:16 +0900

    싱글톤 생성중

--- core/src/test/java/hello/core/singletone/SingletoneService.java
+++ core/src/test/java/hello/core/singletone/SingletoneService.java
@@ -3,4 +3,8 @@ package hello.core.singletone;
 public class SingletoneService {
 
     private static final SingletoneService instance = new SingletoneService();
+
+    public static SingletoneService getInstance() {
+        return instance;
+    }
 }
