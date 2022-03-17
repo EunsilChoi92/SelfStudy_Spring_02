89b0f65bc8cee5b4d21fa2a2f460e157db96730c
EunsilChoi92 <atheung1@naver.com>
2022-03-17 23:39:22 +0900

    싱글톤 테스트

--- core/src/test/java/hello/core/singletone/SingletonTest.java
+++ core/src/test/java/hello/core/singletone/SingletonTest.java
@@ -6,7 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SingletoneTest {
+public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -29,6 +29,10 @@ public class SingletoneTest {
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
     void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
 
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
     }
 }
