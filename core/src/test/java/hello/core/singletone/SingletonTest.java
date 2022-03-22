72e34450ea335e4096fd4e283f6c29b3e1fefc31
EunsilChoi92 <atheung1@naver.com>
2022-03-23 01:47:16 +0900

    싱글톤 컨테이너

--- core/src/test/java/hello/core/singletone/SingletonTest.java
+++ core/src/test/java/hello/core/singletone/SingletonTest.java
@@ -45,20 +45,17 @@ public class SingletonTest {
     @Test
     @DisplayName("스프링 컨테이너와 싱글톤")
     void springContainer() {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-//        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        // 1. 조회: 호출할 때 마다 객체를 생성
-        MemberService memberService1 = appConfig.memberService();
-
-        // 2. 조회: 호출할 때 마다 객체를 생성
-        MemberService memberService2 = appConfig.memberService();
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
         // 참조값이 다르것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
